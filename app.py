244cda9b7abd0e43cc9df41b8af4e4517e9e7528
rishavroy5011-oss <rishavroy5011@gmail.com>
2026-07-07 23:34:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import streamlit as st
 import google.generativeai as genai
 
 # --- ZERO KHARCH AUTOMATIC API SETUP ---
-AQ.Ab8RN6K0XtkMXWKrEuel_pFZVHc0JOQql262hJsOq-bEjrMEXA
+AUTO_API_KEY = "AQ.Ab8RN6K0XtkMXWKrEuel_pFZVHc0JOQql262hJsOq-bEjrMEXA"
+
 
 
 st.set_page_config(page_title="AI Multi-Tool Hub", layout="centered")
